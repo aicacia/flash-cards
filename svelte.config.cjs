fecf22dccd6a3a41c7bd94a773b702bf2a1f81ee
Nathan Faucett <nathanfaucett@gmail.com>
2021-03-21 05:28:23 -0400

    update project

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,10 +1,18 @@
 const sveltePreprocess = require("svelte-preprocess");
+const adapter = require("@sveltejs/adapter-static");
+const pkg = require("./package.json");
+
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   preprocess: sveltePreprocess(),
   kit: {
-    adapter: "@sveltejs/adapter-static",
+    adapter: adapter(),
     target: "#svelte",
+    vite: {
+      ssr: {
+        noExternal: Object.keys(pkg.dependencies || {}),
+      },
+    },
   },
 };
